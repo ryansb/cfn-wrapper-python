6d9796740ac0f150ab1d134690e47adb340ef416
Ryan S. Brown <sb@ryansb.com>
2016-01-29 12:42:51 -0500

    Add packaging

---
+++ setup.py
@@ -0,0 +1,71 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+test_deps = ['mock', 'pytest', 'pytest-cov', 'pytest-xdist']
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='cfn_resource',
+
+    version='0.2.1',
+
+    description='Wrapper decorators for building CloudFormation custom resources',
+    long_description=long_description,
+    url='https://github.com/ryansb/cfn-wrapper-python',
+    author='Ryan Scott Brown',
+    author_email='sb@ryansb.com',
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords='cloudformation aws cloud custom resource amazon',
+
+    py_modules=["cfn_resource"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'test': test_deps,
+    },
+    package_data={},
+    data_files=[],
+    entry_points={},
+)
